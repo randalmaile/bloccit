aa8d496368a79be9e3ce874279f84889721eda79
randalmaile <randalmaile@gmail.com>
2013-12-24 13:23:51 -0800

    tried to add comments to bloccit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
-  attr_accessible :body
+  belongs_to :user
+  attr_accessible :body, :post
+
+  default_scope order('created_at DESC')
+
+  validates :body, length: { minimum: 5 }, presence: true
+  validates :user, presence: true  
 end
---
+++ db/seeds_old.rb
@@ -0,0 +1,82 @@
+require 'faker'
+
+topics = []
+50.times do
+  topics << Topic.create(
+    name: Faker::Lorem.words(rand(1..10)).join(" "),
+    description: Faker::Lorem.paragraph(rand(1..4))
+    )
+  end
+
+rand(10..20).times do
+  password = Faker::Lorem.characters(10)
+  u = User.new(
+    name: Faker::Name.name, 
+    email: Faker::Internet.email, 
+    password: password, 
+    password_confirmation: password)
+  u.skip_confirmation!
+  u.save
+
+  # Note: by calling `User.new` instead of `create`,
+  # we create an instance of a user which isn't saved to the database.
+  # The `skip_confirmation!` method sets the confirmation date
+  # to avoid sending an email. The `save` method updates the database.
+
+rand(20..30).times do
+  topic = topics.first
+  p = u.posts.create(
+    topic: topic,
+    title: Faker::Lorem.words(rand(1..10)).join(" "), 
+    body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
+  # set the created_at to a time within the past year
+  p.update_attribute(:created_at, Time.now - rand(600..31536000))
+  topics.rotate! # add this line to move the first to the last so posts get assigned to each topic
+  end
+end
+
+post_count = Post.count
+User.all.each do |user|
+  rand(20..30).times do
+    p = Post.find(rand(1..post_count))
+    c = user.comments.create(
+      body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"),
+      post: p)
+    c.update_attribute(:created_at, Time.now - rand(600..31536000))
+  end
+end
+
+u = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  password_confirmation: 'helloworld'
+  )
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'admin')
+
+u = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com',
+  password: 'helloworld',
+  password_confirmation: 'helloworld'
+  )
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'moderator')
+
+u = User.new(
+  name: 'Member User',
+  email: 'member@example.com',
+  password: 'helloworld',
+  password_confirmation: 'helloworld'
+  )
+u.skip_confirmation!Crea
+u.save
+
+puts "Seed finished"
+puts "#{Topic.count} topics created"
+puts "#{User.count} users created"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
